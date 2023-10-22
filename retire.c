88b6bfc96d5e08dde80da71f0cd558b3e820291a
bilalmalik101 <144342543+bilalmalik101@users.noreply.github.com>
2023-10-22 22:10:36 +0500

    prompt to user

---
+++ retire.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+int main(){
+    // declaring variables
+     float initial_amount, monthly_contribution_amount, annual_return, annual_inflation, balance;
+     float interest, adjust_inflation_return_rate, total_interest_earned=0;
+     //prompt to user
+     printf("Enter the initial amount:");
+     scanf("%f",&initial_amount);
+     printf("Enter the monthly contributed Money:");
+     scanf("%f",&monthly_contribution_amount);
+     printf("Enter the return rate(scale 0,1):");
+     scanf("%f",&annual_return); 
+     printf("Enter the inflation rate(scale 0,1):");
+     scanf("%f",&annual_inflation); 
+     
+}
