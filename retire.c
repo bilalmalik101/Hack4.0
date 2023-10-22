1ad3310355db0d139011a5cb174c782a0460f5da
bilalmalik101 <144342543+bilalmalik101@users.noreply.github.com>
2023-10-22 22:15:47 +0500

    year convert to month and then find ajust return and inflation rate and using for loop run according the month

--- retire.c
+++ retire.c
@@ -5,6 +5,7 @@ int main(){
     // declaring variables
      float initial_amount, monthly_contribution_amount, annual_return, annual_inflation, balance;
      float interest, adjust_inflation_return_rate, total_interest_earned=0;
+     int monthly,year;
      //prompt to user
      printf("Enter the initial amount:");
      scanf("%f",&initial_amount);
@@ -14,5 +15,11 @@ int main(){
      scanf("%f",&annual_return); 
      printf("Enter the inflation rate(scale 0,1):");
      scanf("%f",&annual_inflation); 
-     
+     monthly=year*12;
+     adjust_inflation_return_rate=((1+annual_return)/(1+annual_inflation))-1;
+     for(int i =1; i<=monthly; i++){
+    
+     }
+
+
 }
