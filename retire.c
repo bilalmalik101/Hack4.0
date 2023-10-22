0a42f07d00d923791a9806c19fe2677978bff59a
bilalmalik101 <144342543+bilalmalik101@users.noreply.github.com>
2023-10-22 22:55:49 +0500

    output

--- retire.c
+++ retire.c
@@ -15,17 +15,19 @@ int main(){
      scanf("%f",&annual_return); 
      printf("Enter the inflation rate(scale 0,1):");
      scanf("%f",&annual_inflation); 
+     printf("Enter the Retirement age:");
+     scanf("%d",&year);
      monthly=year*12;
      printf("|Month|\t|interast    |\t|Amount   |\n");
      adjust_inflation_return_rate=((1+annual_return)/(1+annual_inflation))-1;
      for(int i =1; i<=monthly; i++){
-      interest=(initial_amount * adjust_inflation_return_rate)/2;
+      interest=(initial_amount * adjust_inflation_return_rate)/12;
       initial_amount+=monthly_contribution_amount + interest;
       total_interest_earned+=interest;
       printf("%d\t%f\t%f\n",monthly,interest,initial_amount);
 
      }
+     printf("Total earned interest = %f\n",total_interest_earned);
+     printf("Total nest = %f \n",initial_amount);
     
-
-
 }
